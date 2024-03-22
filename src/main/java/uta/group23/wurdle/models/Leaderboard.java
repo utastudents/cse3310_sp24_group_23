850672ed68a3f6ca1fba168421522b987371a705
n33ka <158865634+n33ka@users.noreply.github.com>
2024-03-22 12:26:06 -0500

    Create Leaderboard.java

---
+++ src/main/java/uta/group23/wurdle/models/Leaderboard.java
@@ -0,0 +1,31 @@
+package uta.group23.wurdle.models;
+
+public class Leaderboard {
+    private User user;
+    private Score[] scores;
+
+    public Leaderboard(User user, Score[] scores) {
+        this.user = user;
+        this.scores = scores;
+    }
+
+    public void storeScores() 
+    {
+
+    }
+
+    public void sortScores() 
+    {
+
+    }
+
+    public void updateScores() 
+    {
+
+    }
+
+    public void displayScores() 
+    {
+        
+    }
+}
