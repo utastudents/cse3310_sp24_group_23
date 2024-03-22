0c721cb695a1b07295c68a6924c5a5804e27ac63
cokeisthebest <158531206+cokeisthebest@users.noreply.github.com>
2024-03-22 18:24:33 -0500

    Create LobbyList.java

---
+++ src/main/java/uta/group23/wurdle/socket/LobbyList.java
@@ -0,0 +1,20 @@
+package uta.group23.wurdle.socket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LobbyList {
+    private List<Lobby> lobbies;
+
+    public LobbyList() {
+        lobbies = new ArrayList<>();
+    }
+
+    public void addLobby(Lobby lobby) {
+        lobbies.add(lobby);
+    }
+
+    public void removeLobby(Lobby lobby) {
+        lobbies.remove(lobby);
+    }
+}
