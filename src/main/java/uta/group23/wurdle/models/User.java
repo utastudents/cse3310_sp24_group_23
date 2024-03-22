9be09941abb1636092c725cf2bb2fa7d54d93c58
n33ka <158865634+n33ka@users.noreply.github.com>
2024-03-22 12:25:10 -0500

    Create User.java

---
+++ src/main/java/uta/group23/wurdle/models/User.java
@@ -0,0 +1,57 @@
+package uta.group23.wurdle.models;
+
+public class User {
+    private String nickname;
+    private int points;
+    private String[] words_found;
+    private Colour user_colour;
+    private int num_found;
+
+    public User(String nickname, int points, String[] words_found, Colour user_colour, int num_found) {
+        this.nickname = nickname;
+        this.points = points;
+        this.words_found = words_found;
+        this.user_colour = user_colour;
+        this.num_found = num_found;
+    }
+
+    public void setNickName(String nickname) 
+    {
+        this.nickname = nickname;
+    }
+
+    public void joinLobby(Lobby lobby) 
+    {
+    
+    }
+
+    public Lobby createLobby() 
+    {
+        return null;
+    }
+
+    public Colour setColour() 
+    {
+        return null;
+    }
+
+    public void leaveLobby() 
+    {
+
+    }
+
+    public boolean isReader() 
+    {
+        return false;
+    }
+
+    public void selectCell(Lobby lobby, int row, int col) 
+    {
+
+    }
+
+    public void sendMessage(String[] message) 
+    {
+
+    }
+}
