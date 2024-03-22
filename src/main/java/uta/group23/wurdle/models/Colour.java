bf6b08f22755b4206ba288a3c91b115e157dd9a6
n33ka <158865634+n33ka@users.noreply.github.com>
2024-03-22 12:32:41 -0500

    Create Colour.java

---
+++ src/main/java/uta/group23/wurdle/models/Colour.java
@@ -0,0 +1,16 @@
+package uta.group23.wurdle.models;
+
+public enum Colour 
+{
+    Red("Red"),
+    Green("Green"),
+    Blue("Blue"),
+    Yellow("Yellow"),
+    Purple("Purple"),
+    Cyan("Cyan");
+
+    public void chanceColour() 
+    {
+
+    }
+}
