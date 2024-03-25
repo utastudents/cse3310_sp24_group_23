9a4a7283e6f1123dc4222bbcefc515450e4972a5
srinivasm17 <svm3005@mavs.uta.edu>
2024-03-25 01:53:58 -0500

    Added mode and status enum classes

---
+++ src/main/java/uta/group23/wurdle/models/Mode.java
@@ -0,0 +1,12 @@
+package uta.group23.wurdle.models;
+
+public class Mode {
+    enum Mode {
+        TIMER,
+        POINT
+    }
+
+    public static void changeMode() {
+
+    }
+}
---
+++ src/main/java/uta/group23/wurdle/models/Status.java
@@ -0,0 +1,13 @@
+package uta.group23.wurdle.models;
+
+public class Status {
+    public enum Status {
+        ONGOING,
+        WAITING,
+        GAMEOVER
+    }
+
+    public static void changeStatus() {
+
+    }
+}
