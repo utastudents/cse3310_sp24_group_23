832358c0341363be4f180b17b0095798d236af53
tdn3411 <tdn3411@mavs.uta.edu>
2024-03-25 23:45:50 -0500

    fix typo and remove User

--- src/main/java/uta/group23/wurdle/models/Leaderboard.java
+++ src/main/java/uta/group23/wurdle/models/Leaderboard.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Leaderboard {
-    private User user;
+    private Player player;
 
     private HashMap<Player, Integer> scores;
 
