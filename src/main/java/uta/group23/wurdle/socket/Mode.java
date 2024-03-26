6f155b48009b7ff4b9502d9eb88eea4f9649454c
srinivasm17 <svm3005@mavs.uta.edu>
2024-03-25 20:31:47 -0500

    deleted mode/status in models, updated in socket.

--- src/main/java/uta/group23/wurdle/socket/Mode.java
+++ src/main/java/uta/group23/wurdle/socket/Mode.java
@@ -1,6 +1,12 @@
 package uta.group23.wurdle.socket;
 
+import uta.group23.wurdle.models.Room;
+
 public enum Mode {
     Timer,
-    Point
+    Point;
+
+    public static void changeMode(Room room) {
+
+    }
 }
--- src/main/java/uta/group23/wurdle/socket/Status.java
+++ src/main/java/uta/group23/wurdle/socket/Status.java
@@ -1,7 +1,13 @@
 package uta.group23.wurdle.socket;
 
+import uta.group23.wurdle.models.Room;
+
 public enum Status {
     WAITING,
     IN_PROGRESS,
-    FINISHED
+    FINISHED;
+
+    public static void changeStatus(Room room) {
+
+    }
 }
