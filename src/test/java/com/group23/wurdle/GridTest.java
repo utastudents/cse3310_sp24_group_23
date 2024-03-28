d8360800370041a2b00cdb5778ec78be2d3c6f01
skynse <austinamakyeansah@gmail.com>
2024-03-28 10:12:26 -0500

    use absolute path for grid tests

--- src/test/java/com/group23/wurdle/GridTest.java
+++ src/test/java/com/group23/wurdle/GridTest.java
@@ -2,6 +2,10 @@ package com.group23.wurdle;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.net.URL;
+
+import org.apache.commons.lang3.ObjectUtils.Null;
 import org.junit.Test;
 
 import uta.group23.wurdle.grid.Grid;
@@ -14,44 +18,42 @@ public class GridTest {
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void testGridGen() {
-        GridGen g = new GridGen("/home/skynse/projects/wurdle/src/main/java/uta/group23/wurdle/words.txt");
-    }
 
     @Test
     public void canFindWord() {
         // might not pass all the time, but is meant to make sure that
         // the word list is being read correctly and actually contains valid words from
         // the list
-        GridGen g = new GridGen("/home/skynse/projects/wurdle/src/main/java/uta/group23/wurdle/words.txt");
+        URL path = GridTest.class.getResource("./words.txt");
+        File f = new File(path.getFile());
+
+        GridGen gen = new GridGen(f.getAbsolutePath());
         Grid grid = new Grid(50, 50);
         try {
-            assertTrue(g.wordExists("worried", grid));
-            assertTrue(!g.wordExists("sndsdisndsf", grid));
+            assertTrue(gen.wordExists("worried", grid));
+            assertTrue(!gen.wordExists("sndsdisndsf", grid));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public void printGrid() throws Exception {
+    public void validGrid() throws Exception {
         Grid g = new Grid(50, 50);
-        GridGen gen = new GridGen("/home/skynse/projects/wurdle/src/main/java/uta/group23/wurdle/words.txt");
+        // wordlist is in same directory as the test
+        URL path = GridTest.class.getResource("./words.txt");
+        File f = new File(path.getFile());
+
+        GridGen gen = new GridGen(f.getAbsolutePath());
         gen.generateGrid(g);
+
+        // printing takes too long, so just make sure every cell is filled
 
         for (int i = 0; i < g.getWidth(); i++) {
             for (int j = 0; j < g.getHeight(); j++) {
-                System.out.print(g.getGrid()[i][j].getLetter() + " ");
+                assertTrue(g.getGrid()[i][j].getLetter() != ' ');
             }
-            System.out.println();
         }
 
     }
-
 }
