686a14f222db66986964ab5ee2a12bd98d2a91af
n33ka <158865634+n33ka@users.noreply.github.com>
2024-04-20 18:57:54 -0500

    Update Leaderboard.java
    sort leaderboard of last game

--- src/main/java/uta/group23/wurdle/models/Leaderboard.java
+++ src/main/java/uta/group23/wurdle/models/Leaderboard.java
@@ -2,6 +2,7 @@ package uta.group23.wurdle.models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.*;
 
 public class Leaderboard {
     private Player player;
@@ -13,11 +14,31 @@ public class Leaderboard {
     }
 
     public void storeScores() {
-
+        scores.put(player, score);
     }
 
-    public void sortScores() {
+    public void sortScores() 
+    {
+        List<Map.Entry<Player, Integer>> scoreList = new ArrayList<>(scores.entrySet());
 
+        //sort the scores in ascending order
+        scoreList.sort(new Comparator<Map.Entry<Player, Integer>>() 
+        {
+            @Override
+            public int compare(Map.Entry<Player, Integer> e1, Map.Entry<Player, Integer> e2) 
+            {
+                return e1.getValue().compareTo(e2.getValue());
+            }
+        });
+        
+        //clear the scores map for next game
+        scores.clear();
+
+        //add the sorted entries back into the scores map
+        for (Map.Entry<Player, Integer> entry : scoreList) 
+        {
+            scores.put(entry.getKey(), entry.getValue());
+        }    
     }
 
     public void updateScores() {
