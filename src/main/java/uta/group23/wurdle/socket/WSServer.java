1a250a28747b559cc6b74726436463f7e04ffe9f
Yashica <158531206+cokeisthebest@users.noreply.github.com>
2024-04-25 19:37:53 -0500

    Update WSServer.java

--- src/main/java/uta/group23/wurdle/socket/WSServer.java
+++ src/main/java/uta/group23/wurdle/socket/WSServer.java
@@ -5,6 +5,7 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import uta.group23.wurdle.models.Context;
+import uta.group23.wurdle.models.Message;
 import uta.group23.wurdle.models.Player;
 
 import java.util.UUID;
@@ -16,6 +17,7 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -31,11 +33,6 @@ public class WSServer extends WebSocketServer {
 
     }
 
-    public void broadCastMessageBoard() {
-        String messages = ctx.getMessageBoard();
-        broadcast(messages);
-    }
-
     public void broadCastLobbyList() {
         String lobbies = ctx.getLobbyList();
         broadcast(lobbies);
@@ -43,149 +40,233 @@ public class WSServer extends WebSocketServer {
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-        JsonObject j = JsonParser.parseString(message).getAsJsonObject(); // convert to json
+        /*
+         * id (1) player add ["data" : {"id": 1, "data": <user information here>}] S->C
+         * id (2) player remove ["data" : {"id": 2, "data": {"id" : <user id>}}]
+         * id (11) server->client message ["data": {"id": 11, "data":}]
+         * id (12) client->server message ["data": {"id": 12, "data":}]
+         * 
+         * id (30) send message ["data",{"id":30,"data":"\<message content\>"}]
+         * id (30) send message (server->client)
+         * ["data",{"id":30,"data":{"id":0,"msg":"test2"}}] id: id of sender, msg:
+         * message content
+         * 
+         */
 
-        if (j.has("type")) {
-            String messageType = j.get("type").getAsString();
-    
-            if (messageType.equals("playerReady")) {
-                // Extract player ID from the message
-                String playerId = j.get("playerId").getAsString();
-                // Broadcast this readiness status update to all clients
-                Player player = ctx.getplayerId(playerId);
+        JsonArray j = JsonParser.parseString(message).getAsJsonArray();
+        System.out.println(j);
+        // check if first key is data
+        // ["join",{"id":"","data":{"username":"test"}}]
+        // ["data",{"id":30,"data":"\<message content\>"}] example
+        // ["join",{"id":"d6576da3-3a90-4fff-a144-4540354804fe","data":{"username":"person"}}]
 
-                if (player != null && !player.isReady()) {
-                    player.setReady(true);
-                broadcastPlayerReady(playerId);
-            }
+        if (j.get(0).getAsString().equals("join")) {
+            JsonObject data = j.get(1).getAsJsonObject();
+            String username = data.get("data").getAsJsonObject().get("username").getAsString();
+            ctx.getPlayerByConn(conn).setNickname(username);
+
+            ctx.addMessage("System", "New player connected: " + username);
+
+            broadcast("[\"data\",{\"id\":30,\"data\":{\"id\":\"system\",\"msg\":\"" + username
+                    + " has joined the game\"}}]");
+
         }
 
-        if (j.get("type").getAsString().equals("message")) {
-            String msg = j.get("content").getAsString();
 
-            // add to message board
-            ctx.addMessage(ctx.getPlayerByConn(conn).getNickname(), msg);
-            // broadcast messageBoard to all clients
-            /**
-             * messageBoard: [
-             * {"username": "Player 1", "message": "Hello, is anyone here?"},]
+        if (j.get(0).getAsString().equals("leave")) { // leave lobby
+
+            ctx.leaveLobby(conn);
+            broadCastLobbyList();
+        }
+
+        if (j.get(0).getAsString().equals("create")) {
+            /*
+             * JSON.stringify([
+             * "create",
+             * {
+             * id: clientState.uuid,
+             * data: {
+             * name: document.getElementById("lobby-name").value,
+             * owner: clientState.uuid,
+             * players: [],
+             * playerCount: document.getElementById("player-count").value,
+             * lobbyMode: document.getElementById("game-type").value,
+             * password: document.getElementById("lobby-password").value,
+             * },
+             * },
+             * ]);
              */
+            // create lobby
+            JsonObject data = j.get(1).getAsJsonObject();
+            String lobbyName = data.get("data").getAsJsonObject().get("name").getAsString();
+            String lobbyID = UUID.randomUUID().toString();
+            String password = data.get("data").getAsJsonObject().get("password").getAsString();
+            int playerCap = data.get("data").getAsJsonObject().get("playerCount").getAsInt();
+            String mode = data.get("data").getAsJsonObject().get("lobbyMode").getAsString();
+            String modeStr = mode.equals("timer") ? "Timer" : "Point";
+            Mode lobbyMode = Mode.valueOf(modeStr);
+            Player lobbyOwner = ctx.getPlayerByConn(conn);
+            Lobby lobby = new Lobby(lobbyName, lobbyID, Status.WAITING, 0, lobbyMode, password, playerCap, lobbyOwner);
+            lobby.addPlayer(lobbyOwner);
 
-            broadCastMessageBoard();
+            // send data back to creator
+            String lobbyData = "[\"data\",{\"id\":11,\"data\":" + "{\"id\":1,\"data\":"
+                    + lobby.toJsonObjectPrivate().toString()
+                    + "}}]";
 
-        }
+            System.out.println(lobbyData);
+            conn.send(lobbyData);
 
-        broadCastLobbyList();
-
-        if (j.get("type").getAsString().equals("setUsername")) {
-            setUsername(conn, j);
-        }
-
-        if (j.get("type").getAsString().equals("createLobby")) {
-            createLobby(conn, j);
-        }
-
-        if (j.get("type").getAsString().equals("leaveLobby")) {
-            leaveLobby(conn, j);
-        }
-
-        if (j.get("type").getAsString().equals("startGame")) {
-            String lobbyID = j.get("lobbyID").getAsString();
-            Lobby lobby = ctx.searchID(lobbyID);
-            lobby.startGame();
-        }
-
-        if (j.get("type").getAsString().equals("joinLobby")) {
-
-            String lobbyID = j.get("lobbyId").getAsString();
-            Lobby lobby = ctx.searchID(lobbyID);
-
-            if (j.get("password") != null) {
-                String password = j.get("password").getAsString();
-                if (!lobby.getPassword().equals(password)) {
-                    return;
-                }
-            }
-
-            Player player = ctx.getPlayerByConn(conn);
-            lobby.addPlayer(player);
-
-            // broadcast lobby info to all clients of this lobby
-
-            for (Player p : lobby.getPlayers()) {
-                String data = "{\"type\": \"lobbyUpdate\", \"lobby\": " + lobby.toJsonObjectPrivate().toString() + "}";
-                System.out.println(data);
-                p.getClient().getConn().send(data);
-            }
+            ctx.addLobby(lobby, lobbyOwner);
 
             broadCastLobbyList();
         }
 
-
-    }
-    private void broadcastPlayerReady(String playerId){
-        JsonObject msg= new JsonObject();
-        msg.addProperty("type", "playerReady");
-        msg.addProperty("playerId", playerId);
-    
-        broadcast(msg.toString());
+        if (j.get(0).getAsString().equals("data")) {
+            JsonObject data = j.get(1).getAsJsonObject();
+            switch (data.get("id").getAsInt()) {
+                case 30:
+                    // Assuming message type is a word selection
+                    String msgData = data.get("data").getAsJsonObject().get("msg").getAsString();
+                    String[] selectedCells = msgData.split(","); // Example: "A1,B2,C3" => ["A1", "B2", "C3"]
         
-    } 
+                    Player player = ctx.getPlayerByConn(conn);
+                    if (player != null) {
+                        game.checkWord(player, selectedCells); // Check word validity and assign points
+                    }
+                    broadcast(ctx.getMessageBoard());
+                    break;
+        
+            }
+        }
+        if (j.get(0).getAsString().equals("data")) {
+            JsonObject data = j.get(1).getAsJsonObject();
+            switch (data.get("id").getAsInt()) {
+                case 11:
+                    // server->client message
+                    break;
+                case 12:
+                    // client->server message
+                    /*
+                     * [
+                     * "data",
+                     * {
+                     * id: 12, // C->S
+                     * data: {
+                     * id: 1, // join lobby
+                     * data: {
+                     * id: id,
+                     * },
+                     * },
+                     * },
+                     * ]
+                     * 
+                     */
 
+                    JsonObject subData = data.get("data").getAsJsonObject();
+                    int subId = subData.get("id").getAsInt();
 
-    private void setUsername(WebSocket conn, JsonObject j) {
-        String username = j.get("username").getAsString();
-        // ctx.getPlayerByConn(conn).setNickname(username);
-        if (ctx.isUsernameTaken(username)) {
-            /* type: "usernameQuery", accepted: false */
-            conn.send("{\"type\": \"usernameQuery\", \"accepted\": false}");
+                    if (subId == 1) {
+                        System.out.println("Joining lobby");
+                        // join lobby
+                        String lobbyID = subData.get("data").getAsJsonObject().get("id").getAsString();
+                        Lobby lobby = ctx.searchID(lobbyID);
+                        Player player = ctx.getPlayerByConn(conn);
+                        lobby.addPlayer(player);
+
+                        // broadcast lobby info to all clients of this lobby
+                        for (Player p : lobby.getPlayers()) {
+                            String lobbyData = "[\"data\",{\"id\":11,\"data\":" + "{\"id\":1,\"data\":"
+                                    + lobby.toJsonObjectPrivate().toString() + "}}]";
+
+                            System.out.println(lobbyData);
+                            p.getConn().send(lobbyData);
+                        }
+
+                        broadCastLobbyList();
+                    }
+
+                    break;
+                case 30:
+                    // send message
+
+                    String msgData = data.get("data").getAsJsonObject().get("msg").getAsString();
+                    Player player = ctx.getPlayerByConn(conn);
+                    String msg = msgData;
+                    ctx.addMessage(player.getNickname(), msg);
+                    // broadCastMessageBoard();
+
+                    broadcast(ctx.getMessageBoard());
+                    break;
+                default:
+                    break;
+            }
+        } else {
             return;
         }
 
-        conn.send("{\"type\": \"usernameQuery\", \"accepted\": true}");
-        ctx.getPlayerByConn(conn).setNickname(username);
-        ctx.addMessage("System", "New player connected: " + username);
-        broadCastMessageBoard();
-    }
-
-    private void leaveLobby(WebSocket conn, JsonObject j) {
-        String lobbyID = j.get("lobbyID").getAsString();
-        Lobby lobby = ctx.searchID(lobbyID);
-        Player player = ctx.getPlayerByConn(conn);
-        lobby.removePlayer(player);
-
-        // broadcast lobby info to all clients of this lobby
-        for (Player p : lobby.getPlayers()) {
-            String data = "{\"type\": \"lobbyUpdate\", \"lobby\": " + lobby.toJsonObjectPrivate().toString() + "}";
-            p.getClient().getConn().send(data);
-        }
-
-        broadCastLobbyList();
-    }
-
-    private void createLobby(WebSocket conn, JsonObject j) {
-        String lobbyName = j.get("lobbyName").getAsString();
-        String lobbyID = UUID.randomUUID().toString();
-        String password = "";
-
-        int playerCap = j.get("playerCount").getAsInt();
-
-        if (j.get("password") != null) {
-            password = j.get("password").getAsString();
-        }
-
-        String mode = j.get("lobbyMode").getAsString();
-        System.out.println(1);
-        String modeStr = mode.equals("timer") ? "Timer" : "Point";
-        Mode lobbyMode = Mode.valueOf(modeStr);
-        Player lobbyOwner = ctx.getPlayerByConn(conn);
-        Lobby lobby = new Lobby(lobbyName, lobbyID, Status.WAITING, 0, lobbyMode, password, playerCap, lobbyOwner);
-
-        ctx.addLobby(lobby, lobbyOwner);
-
-        String data = "{\"type\": \"lobbyUpdate\", \"lobby\": " + lobby.toJsonObjectPrivate().toString() + "}";
-        conn.send(data);
-        broadCastLobbyList();
+        /**
+         * if (j.get("type").getAsString().equals("message")) {
+         * String msg = j.get("content").getAsString();
+         * 
+         * // add to message board
+         * ctx.addMessage(ctx.getPlayerByConn(conn).getNickname(), msg);
+         * // broadcast messageBoard to all clients
+         * /**
+         * messageBoard: [
+         * {"username": "Player 1", "message": "Hello, is anyone here?"},]
+         * 
+         * 
+         * broadCastMessageBoard();
+         * 
+         * }
+         * 
+         * broadCastLobbyList();
+         * 
+         * if (j.get("type").getAsString().equals("setUsername")) {
+         * setUsername(conn, j);
+         * }
+         * 
+         * if (j.get("type").getAsString().equals("createLobby")) {
+         * createLobby(conn, j);
+         * }
+         * 
+         * if (j.get("type").getAsString().equals("leaveLobby")) {
+         * leaveLobby(conn, j);
+         * }
+         * 
+         * if (j.get("type").getAsString().equals("startGame")) {
+         * String lobbyID = j.get("lobbyID").getAsString();
+         * Lobby lobby = ctx.searchID(lobbyID);
+         * lobby.startGame();
+         * }
+         * 
+         * if (j.get("type").getAsString().equals("joinLobby")) {
+         * 
+         * String lobbyID = j.get("lobbyId").getAsString();
+         * Lobby lobby = ctx.searchID(lobbyID);
+         * 
+         * if (j.get("password") != null) {
+         * String password = j.get("password").getAsString();
+         * if (!lobby.getPassword().equals(password)) {
+         * return;
+         * }
+         * }
+         * 
+         * Player player = ctx.getPlayerByConn(conn);
+         * lobby.addPlayer(player);
+         * 
+         * // broadcast lobby info to all clients of this lobby
+         * 
+         * for (Player p : lobby.getPlayers()) {
+         * String data = "{\"type\": \"lobbyUpdate\", \"lobby\": " +
+         * lobby.toJsonObjectPrivate().toString() + "}";
+         * System.out.println(data);
+         * p.getClient().getConn().send(data);
+         * }
+         * 
+         * broadCastLobbyList();
+         */
 
     }
 
@@ -210,21 +291,27 @@ public class WSServer extends WebSocketServer {
         ctx.addPlayer(newPlayer);
         System.out.println("Client count: " + ctx.getPlayerSize());
         // send selfID to client
-        conn.send("{\"type\": \"selfID\", \"id\": \"" + newId + "\"}");
+        // conn.send("{\"type\": \"selfID\", \"id\": \"" + newId + "\"}");
+        conn.send("[\"data\",{\"id\":1,\"data\":{\"id\":\"" + newId + "\"}}]");
+
+        conn.send(ctx.getMessageBoard());
+        conn.send(ctx.getLobbyList());
     }
 
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
         // TODO Auto-generated method stub
         System.out.println("Connection closed : " + conn.getResourceDescriptor());
-        
-        ctx.addMessage("System", "Player '"+ ctx.getPlayerByConn(conn).getNickname() + "' has disconnected");
-        broadCastMessageBoard();
-        
+
+        ctx.addMessage("System", "Player '" + ctx.getPlayerByConn(conn).getNickname() + "' has disconnected");
+        // last message as json
+        broadcast(ctx.getMessageBoard());
+
         ctx.removePlayer(conn);
         System.out.println("Client count: " + ctx.getPlayerSize());
 
-        
+        broadCastLobbyList();
+
     }
 
     @Override
