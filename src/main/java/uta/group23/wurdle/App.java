b83010b706da9ffec62b5de06a0a2aad3132fabc
n33ka <158865634+n33ka@users.noreply.github.com>
2024-04-28 20:48:33 -0500

    add start instance

--- src/main/java/uta/group23/wurdle/App.java
+++ src/main/java/uta/group23/wurdle/App.java
@@ -8,6 +8,7 @@ import uta.group23.wurdle.models.Context;
 
 import uta.group23.wurdle.server.HTTPServer;
 import uta.group23.wurdle.socket.*;
+import uta.group23.wurdle.Game;
 
 public class App {
 
@@ -21,6 +22,20 @@ public class App {
             WSServer socketServer = new WSServer("127.0.0.1", new InetSocketAddress(9123), ctx);
             socketServer.setReuseAddr(true);
             socketServer.start();
+
+            // Check if game can start
+            int playerCount = ctx.getPlayerSize();
+            boolean possiblestart = false;
+
+            if (playerCount >= 2 && playerCount <= 4) 
+            {
+                possiblestart = true;
+            }
+
+            if (possiblestart) {
+                Game game = new Game();
+                game.start();
+              }
 
         } catch (Exception e) {
             e.printStackTrace();
